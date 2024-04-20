e5bf19383d84e4ea8406d54ea3712a33ba886e43
Alberto Martinez <alberto@intelligential.tech>
2024-04-20 13:25:35 -0600

    [sidbar] finish animations

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,3 +1,31 @@
+import { motion } from 'framer-motion';
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1
+    },
+  },
+}
+
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+}
+
 const Link = () => {
   const items = [
     "Homepage",
@@ -7,13 +35,20 @@ const Link = () => {
     "About"
   ]
   return (
-    <div className="links">
+    <motion.div className="links" variants={variants}>
       {
         items.map(item => (
-          <a key={item} href={`#${item}`}>{item}</a>
+          <motion.a 
+            key={item}
+            href={`#${item}`}
+            variants={itemVariants}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}>
+              {item}
+          </motion.a>
         ))
       }
-    </div>
+    </motion.div>
   );
 }
 
